b02b3705dcc5aba421032cb9fced217d941cfe18
Kevin Paul <kpjtofficial@gmail.com>
2022-11-11 19:14:04 +0530

    Changes to CSS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { AiOutlinePlus } from 'react-icons/ai'
 
 const styles = {
   bg: `min-h-screen w-screen overflow-x-hidden p-4 bg-gradient-to-r from-[#141e30] to-[#243b55]`,
-  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4 mt-20`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4 my-20`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form : `flex justify-between`,
   input: `border p-2 w-full text-xl`,
