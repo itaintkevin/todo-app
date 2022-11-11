6f6a0511a0b67c2237a7372fc45efd7775b817d0
Kevin Paul <kpjtofficial@gmail.com>
2022-11-11 19:12:39 +0530

    Changes to CSS and changed App Title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Todo from './components/todo'
 import { AiOutlinePlus } from 'react-icons/ai'
 
 const styles = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#141e30] to-[#243b55]`,
+  bg: `min-h-screen min-w-screen p-4 bg-gradient-to-r from-[#141e30] to-[#243b55]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4 mt-20`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form : `flex justify-between`,
